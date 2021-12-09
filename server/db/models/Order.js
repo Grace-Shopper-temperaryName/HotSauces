defc674bbab2ab7d3ecb1d0601568ad766fe3d0b
Anthony Liang <antliang1117@gmail.com>
2021-12-09 16:08:41 -0500

    Feature: update Customer, HotSauce, Order, OrderHotSauce models and seed file,  delete Payment model

--- server/db/models/Order.js
+++ server/db/models/Order.js
@@ -3,7 +3,7 @@ const db = require("../db");
 
 const Order = db.define("order", {
   orderStatus: {
-    // type: Sequelize.ENUM()
+    // type: Sequelize.ENUM('cancelled', 'opened' , 'completed')
     type: Sequelize.BOOLEAN,
     allowNull: false,
     validate: {
@@ -14,6 +14,33 @@ const Order = db.define("order", {
     type: Sequelize.DATE,
   },
   isCart: {
+    // true = orderStatus: opened , false = orderStatus: completed or cancelled
+    type: Sequelize.BOOLEAN,
+  },
+  amount: {
+    type: Sequelize.INTEGER,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  provider: {
+    // type: Sequelize.ENUM("Visa", "Mastercard", "American Express", "Discover"),
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  cardNumber: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      notEmpty: true,
+    },
+  },
+  paymentStatus: {
+    // type: Sequelize.ENUM("cancelled", "pending", "fulfilled" )
     type: Sequelize.BOOLEAN,
   },
 });
