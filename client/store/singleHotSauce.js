722e9e6536f14a595966c48c7f56d4fa4aa9b7c3
Heather White <miss.pomykal@gmail.com>
2021-12-10 15:56:57 -0500

    UPDATE: EDIT singleHotSauce action type, creator, thunk and reducer done

--- client/store/singleHotSauce.js
+++ client/store/singleHotSauce.js
@@ -1,7 +1,9 @@
-import axios from 'axios';
-import history from '../history';
+import axios from "axios";
+import history from "../history";
 
-const SET_SINGLE_HOT_SAUCE = 'SET_SINGLE_HOT_SAUCE';
+const SET_SINGLE_HOT_SAUCE = "SET_SINGLE_HOT_SAUCE";
+
+const EDIT_SINGLE_HOT_SAUCE = "EDIT_SINGLE_HOT_SAUCE";
 
 export const setSingleHotSauce = (hotSauce) => {
   return {
@@ -9,6 +11,11 @@ export const setSingleHotSauce = (hotSauce) => {
     hotSauce,
   };
 };
+
+export const _editSingleHotSauce = (hotSauce) => ({
+  type: EDIT_SINGLE_HOT_SAUCE,
+  hotSauce,
+});
 
 export const fetchSingleHotSauce = (id) => {
   return async (dispatch) => {
@@ -21,9 +28,23 @@ export const fetchSingleHotSauce = (id) => {
   };
 };
 
+export const editSingleHotSauce = (id, history) => {
+  return async (dispatch) => {
+    try {
+      const { data: hotSauce } = await axios.put(`/api/hotsauces/${id}`);
+      dispatch(_editSingleHotSauce(hotSauce));
+      history.push(`/hotsauces/${id}`);
+    } catch (err) {
+      console.error(err);
+    }
+  };
+};
+
 export default function (state = [], action) {
   switch (action.type) {
     case SET_SINGLE_HOT_SAUCE:
+      return action.hotSauce;
+    case EDIT_SINGLE_HOT_SAUCE:
       return action.hotSauce;
     default:
       return state;
