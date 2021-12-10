e8a3c8605e6952076928f797a72c837ac4d9e023
Heather White <miss.pomykal@gmail.com>
2021-12-10 12:27:33 -0500

    API: routes done for POST, PUT, DELETE hotSauce

--- server/api/hotsauces.js
+++ server/api/hotsauces.js
@@ -1,11 +1,11 @@
-const router = require('express').Router();
+const router = require("express").Router();
 const {
   models: { HotSauce },
-} = require('../db');
+} = require("../db");
 module.exports = router;
 
 // mounted at /api/hotsauces
-router.get('/', async (req, res, next) => {
+router.get("/", async (req, res, next) => {
   try {
     const hotSauces = await HotSauce.findAll();
     res.json(hotSauces);
@@ -14,7 +14,7 @@ router.get('/', async (req, res, next) => {
   }
 });
 
-router.get('/:id', async (req, res, next) => {
+router.get("/:id", async (req, res, next) => {
   try {
     const hotSauce = await HotSauce.findByPk(req.params.id);
     res.json(hotSauce);
@@ -22,3 +22,32 @@ router.get('/:id', async (req, res, next) => {
     next(error);
   }
 });
+
+router.post("/", async (req, res, next) => {
+  try {
+    const newHotSauce = await HotSauce.create(req.body);
+    res.json(newHotSauce);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.put("/:id", async (req, res, next) => {
+  try {
+    const hotSauceToUpdate = await HotSauce.findByPk(req.params.id);
+    const updatedHotSauce = await hotSauceToUpdate.update(req.body);
+    res.json(updatedHotSauce);
+  } catch (error) {
+    next(error);
+  }
+});
+
+router.delete("/:id", async (req, res, next) => {
+  try {
+    const hotSauceToRemove = await HotSauce.findByPk(req.params.id);
+    const removedHotSauce = await hotSauceToRemove.destroy();
+    res.json(removedHotSauce);
+  } catch (error) {
+    next(error);
+  }
+})
