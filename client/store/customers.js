15019b9beb138efab1573230795fd84e74a89892
rblanchard719 <rblanchard719@gmail.com>
2021-12-13 14:52:27 -0500

    UPDATE: place single customer in customers

--- client/store/customers.js
+++ client/store/customers.js
@@ -5,12 +5,19 @@ import history from "../history";
  * ACTION TYPES
  */
 const SET_CUSTOMERS = "SET_CUSTOMERS";
+const CREATE_SINGLE_CUSTOMER = "CREATE_SINGLE_CUSTOMER";
 
 /**
  * ACTION CREATORS
  */
 const setCustomers = (customers) => ({ type: SET_CUSTOMERS, customers });
 
+const createSingleCustomer = (customer) => {
+  return {
+    type: CREATE_SINGLE_CUSTOMER,
+    customer,
+  };
+};
 /**
  * THUNK CREATORS
  */
@@ -25,12 +32,26 @@ export const fetchCustomers = () => {
   };
 };
 
+export const createYourSingleCustomer = (customer) => {
+  return async (dispatch) => {
+    try {
+      const { data } = await axios.post("/api/customers", customer);
+      dispatch(createSingleCustomer(data));
+      history.push("/home");
+    } catch (err) {
+      console.error(err);
+    }
+  };
+};
+
 /**
  * REDUCER
  */
 export default function (state = [], action) {
   switch (action.type) {
     case SET_CUSTOMERS:
+      return action.customers;
+    case CREATE_CUSTOMERS:
       return action.customers;
     default:
       return state;
--- client/store/singleCustomer.js
+++ client/store/singleCustomer.js
@@ -2,18 +2,10 @@ import axios from "axios";
 import history from "../history";
 
 const SET_SINGLE_CUSTOMER = "SET_SINGLE_CUSTOMER";
-const CREATE_SINGLE_CUSTOMER = "CREATE_SINGLE_CUSTOMER";
 
 export const setSingleCustomer = (customer) => {
   return {
     type: SET_SINGLE_CUSTOMER,
-    customer,
-  };
-};
-
-export const createSingleCustomer = (customer) => {
-  return {
-    type: CREATE_SINGLE_CUSTOMER,
     customer,
   };
 };
@@ -29,24 +21,11 @@ export const fetchSingleCustomer = (id) => {
   };
 };
 
-export const createYourSingleCustomer = (customer) => {
-  return async (dispatch) => {
-    try {
-      const { data } = await axios.post("/api/customers", customer);
-      dispatch(createSingleCustomer(data));
-      history.push("/home");
-    } catch (err) {
-      console.error(err);
-    }
-  };
-};
-
 export default function (state = {}, action) {
   switch (action.type) {
     case SET_SINGLE_CUSTOMER:
       return action.customer;
-    case SET_SINGLE_CUSTOMER:
-      return action.customer;
+
     default:
       return state;
   }
