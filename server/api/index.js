5670ce0abb7ba9fb62ee4a7df3c6b4dc12ceeb4d
rblanchard719 <rblanchard719@gmail.com>
2021-12-13 09:11:11 -0500

    FEATURE: add api route for orders;  to get all orders and one order

--- server/api/index.js
+++ server/api/index.js
@@ -1,11 +1,12 @@
-const router = require('express').Router();
+const router = require("express").Router();
 module.exports = router;
 
-router.use('/customers', require('./customers'));
-router.use('/hotsauces', require('./hotsauces'));
+router.use("/customers", require("./customers"));
+router.use("/hotsauces", require("./hotsauces"));
+router.use("/orders", require("./orders"));
 
 router.use((req, res, next) => {
-  const error = new Error('Not Found');
+  const error = new Error("Not Found");
   error.status = 404;
   next(error);
 });
