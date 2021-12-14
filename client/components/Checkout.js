7cf0516d5858da448be96e78bc6b366af3936bae
Heather White <miss.pomykal@gmail.com>
2021-12-14 18:33:20 -0500

    ADD: add Checkout component route. add basic ternary on form for guest/authCustomer

--- client/components/Checkout.js
+++ client/components/Checkout.js
@@ -2,6 +2,7 @@ import { Component } from "react";
 import { connect } from "react-redux";
 import { Link } from "react-router-dom";
 import { me } from "../store/auth";
+import Cart from "./Cart";
 /*
 loggedIn customer info: populated form for delivery address, payment info,
 guest customer: blank form for delivery address, payment info
@@ -57,14 +58,109 @@ export class Checkout extends Component {
       zip,
       provider,
       cardNumber,
-    } = this.state;
+    } = this.state || {};
+    const {
+      cFirstName,
+      cLastName,
+      cPhone,
+      cEmail,
+      cStreetAddress,
+      cCity,
+      cState,
+      cZip,
+    //   cProvider,
+    //   cCardNumber,
+    } = this.props.customer || {};
     const { handleChange, handleSubmit, handleSelect } = this;
     //if guest- use input from this.state
     //if auth customer- populate form from this.props
     return (
       //ADD cart here above form?  <Cart insideCheckout={true} />
       <>
+        <Cart insideCheckout={true} />
+        
         <div>
+        {this.props.customer ? <form method="post" onSubmit={handleSubmit}>
+            <label>Contact Information</label>
+            <input
+              placeholder="Email"
+              name="email"
+              onChange={handleChange}
+              value={cEmail}
+            />
+
+            <label>Shipping Information</label>
+            <span>
+              <input
+                placeholder="First name"
+                name="firstName"
+                onChange={handleChange}
+                value={cFirstName}
+              />
+
+              <input
+                placeholder="Last name"
+                name="lastName"
+                onChange={handleChange}
+                value={cLastName}
+              />
+            </span>
+
+            <input
+              placeholder="Street Address"
+              name="streetAddress"
+              onChange={handleChange}
+              value={cStreetAddress}
+            />
+
+            <input
+              placeholder="City"
+              name="city"
+              onChange={handleChange}
+              value={cCity}
+            />
+
+            <span>
+              <input
+                placeholder="State"
+                name="state"
+                onChange={handleChange}
+                value={cState}
+              />
+
+              <input
+                placeholder="ZIP code"
+                name="zip"
+                onChange={handleChange}
+                value={cZip}
+              />
+            </span>
+
+            <input
+              placeholder="Phone"
+              name="phone"
+              onChange={handleChange}
+              value={cPhone}
+            />
+
+            <label>Payment Information</label>
+            <select
+              placeholder="Credit card provider"
+              name="provider"
+              onChange={handleSelect}
+            >
+              <option disabled>Card type</option>
+              <option value={provider}>American Express</option>
+              <option value={provider}>Mastercard</option>
+              <option value={provider}>Visa</option>
+            </select>
+
+            <input
+              placeholder="Card number"
+              name="cardNumber"
+              value={cardNumber}
+            />
+          </form> :   
           <form method="post" onSubmit={handleSubmit}>
             <label>Contact Information</label>
             <input
@@ -112,7 +208,7 @@ export class Checkout extends Component {
                 onChange={handleChange}
                 value={state}
               />
-              
+
               <input
                 placeholder="ZIP code"
                 name="zip"
@@ -146,7 +242,7 @@ export class Checkout extends Component {
               value={cardNumber}
             />
           </form>
-
+        }
           <button type="submit">Place Order</button>
         </div>
       </>
@@ -157,7 +253,7 @@ export class Checkout extends Component {
 
 const mapState = (state) => {
   return {
-    auth: state.auth,
+    customer: state.auth,
   };
 };
 const mapDispatch = (dispatch) => {
