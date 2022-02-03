a2b68fdc46c8349862524312c416fd0337f420ca
Anthony Liang <antliang1117@gmail.com>
2022-02-03 12:25:32 -0500

    style: change image src to match image in public folder

--- client/components/LandingPage.js
+++ client/components/LandingPage.js
@@ -1,14 +1,14 @@
-import React from "react";
-import { Link } from "react-router-dom";
+import React from 'react';
+import { Link } from 'react-router-dom';
 
 class LandingPage extends React.Component {
   render() {
     return (
-      <div className="landingpage">
-        <img src="home.jpg" />
-        <div className="text-box">
+      <div className='landingpage'>
+        <img src='home.JPG' />
+        <div className='text-box'>
           <h1> Simple, Natural, Hot Sauce </h1>
-          <Link to="/hotsauces">
+          <Link to='/hotsauces'>
             <button>See Our Collection Here</button>
           </Link>
         </div>
